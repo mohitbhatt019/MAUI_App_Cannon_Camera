5181c462688275fa6f47720cc49524152535ba98
Mohit bhatt <Mohitbhatt433@gmail.com>
2024-10-28 17:14:26 +0530

    Setup project

---
+++ MAUI_Cam/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_Cam
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
